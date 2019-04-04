a65255cfcfad021b9413f94c45f1cac967b6078a
Asia <Joanna.gromadzka@seattlecentral.edu>
2019-04-04 14:30:42 -0700

    my 1st push

---
+++ public/index2.js
@@ -0,0 +1,5 @@
+const http = require("http");  
+http.createServer((req,res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Aloha world');
+}).listen(process.env.PORT || 3000);
