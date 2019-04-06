aabbda8eda98addb09986e901faef893d17460e7
Asia <Joanna.gromadzka@seattlecentral.edu>
2019-04-06 15:36:52 -0700

    initial commit

--- public/index2.js
+++
@@ -1,5 +0,0 @@
-const http = require("http");  
-http.createServer((req,res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Aloha world');
-}).listen(process.env.PORT || 3000);
