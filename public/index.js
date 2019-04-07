caedf6a6002125af80c4382d5defd2a9c1b87fae
asiagromasia <35482401+asiagromasia@users.noreply.github.com>
2019-04-06 19:11:56 -0700

    add files upload

---
+++ public/index.js
@@ -0,0 +1,18 @@
+const http = require("http"); 
+http.createServer((req,res) => {
+  const path = req.url.toLowerCase();
+  switch(path) {
+    case '/':
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.end('Home page');
+      break;
+    case '/about':
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.end('About page');
+      break;
+    default:
+      res.writeHead(404, {'Content-Type': 'text/plain'});
+      res.end('Not found');
+      break;
+    }
+}).listen(process.env.PORT || 3000);
