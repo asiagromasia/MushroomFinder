4fbf53a38caef9a9e267f65f0bf2b9052d51b64a
Asia <Joanna.gromadzka@seattlecentral.edu>
2019-04-06 20:59:51 -0700

    adjust title

--- public/index.js
+++ public/index.js
@@ -4,7 +4,7 @@ http.createServer((req,res) => {
   switch(path) {
     case '/':
       res.writeHead(200, {'Content-Type': 'text/plain'});
-      res.end('Welcome to Home page');
+      res.end('Welcome to Home page!!!!!');
       break;
     case '/about':
       res.writeHead(200, {'Content-Type': 'text/plain'});
