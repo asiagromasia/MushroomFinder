35bd8f5fd3c54430f57c12138420345852404bc0
Asia <Joanna.gromadzka@seattlecentral.edu>
2019-04-06 21:20:21 -0700

    extra about

--- public/index.js
+++ public/index.js
@@ -8,7 +8,7 @@ http.createServer((req,res) => {
       break;
     case '/about':
       res.writeHead(200, {'Content-Type': 'text/plain'});
-      res.end('About page');
+      res.end('Welcome to about page!');
       break;
     default:
       res.writeHead(404, {'Content-Type': 'text/plain'});
