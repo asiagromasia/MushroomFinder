48e5f3d53bd3c040a2308cc9f22726f74151c5d6
Asia <Joanna.gromadzka@seattlecentral.edu>
2019-04-24 17:29:46 -0700

    a3 init

--- index.js
+++ index.js
@@ -1,75 +1,65 @@
 'use strict'
-const http = require("http");
-const fs = require("fs"); 
-const qs = require("querystring");
-const mushrooms = require("./lib/mushrooms.js");
+let mushrooms = require("./lib/mushrooms.js");
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
 
-function serveStatic(res, path, contentType, responseCode){
-  if(!responseCode) responseCode = 200;
-  fs.readFile(__dirname + path, function(err, data){
-      if(err){
-        res.writeHead(500, {'Content-Type': 'text/plain'});
-        res.end('Internal Server Error');
-      }
-      else{
-        res.writeHead(responseCode, {'Content-Type': contentType});
-        res.end(data);
-      }
-  });
-}
+app.set('port', process.env.PORT || 3000);
+app.use(express.static(__dirname + '/public')); // set location for static files
+app.use(bodyParser.urlencoded({extended: true})); // parse form submissions
 
+let handlebars =  require("express-handlebars");
+app.engine(".html", handlebars({extname: '.html'}));
+app.set("view engine", ".html");
 
-http.createServer((req,res) => {
-    
-   // separate route from query string
-    let url = req.url.split("?"); 
-    // convert query string to object
-    let query = qs.parse(url[1]); 
-    let path = url[0].toLowerCase();
-    
-  switch(path) {
-    case '/':
-      res.writeHead(200, {'Content-Type': 'text/plain'});
-      //res.end('Welcome to Home page!!!!!');
-        serveStatic(res, '/public/home.html', 'text/html');
-      break;
-          
-    case '/about':
-      res.writeHead(200, {'Content-Type': 'text/plain'});
-      res.end('Welcome to page about mushrooms!');
-      break;
-          
-    case '/getall':
+// send static file as response
+app.get('/', (req, res) => {
+ res.type('text/html');
+ res.sendFile(__dirname + '/public/home.html'); 
+  //  res.render('home');
+});
+
+// send plain text response
+app.get('/about', (req, res) => {
+ res.type('text/plain');
+ res.sendFile(__dirname + '/public/about.html');
+});
+
+app.get('/getall', (req,res) => {
           let mushrCont = mushrooms.getAll();
           res.writeHead(200, {'Content-Type': 'text/plain'});
           res.end('Here are all mushrooms: ' + "\n" + JSON.stringify(mushrCont));
-     break;      
-          
-    case '/get':
-        let found = mushrooms.get(query.name.toLowerCase()) //get mushroom object
-        let results = (found) ? JSON.stringify(found) : " Item Not found";
-        res.writeHead(200, {'Content-Type': 'text/plain'});
-        res.end("Results for mushroom name: " + query.name + "\n" + results);
-      break;   
-            
-    case '/delete':
-        let delMush = mushrooms.delete(query.name.toLowerCase()); 
-        res.writeHead(200, {'Content-Type': 'text/plain'});
-        res.end('Following are results of deleting'+" "+ query.name +' '+ 'mushroom: ' + "\n" + JSON.stringify(delMush));
-      break;
-          
-    case '/add':
-        let adMush = mushrooms.add(query.name.toLowerCase())
-        res.writeHead(200, {'Content-Type': 'text/plain'});
-        res.end('You just added following mushroom:' + query.name + ' '+ JSON.stringify(adMush) );
-      break;
-          
-    default:
-      res.writeHead(404, {'Content-Type': 'text/plain'});
-      res.end('Not found');
-      break;
-          
-    }
-}).listen(process.env.PORT || 3000);
+});      
 
+// handle GET 
+app.get('/delete', (req,res) => {
+    let delMush = mushrooms.delete(req.query.name); // delete book object
+    res.render('delete', {title: req.query.name, result: delMush});
+});
+
+// handle POST
+app.post('/search', (req,res) => {
+    res.type('text/html');
+    console.log(req.body) //display parsed form submission
+    var header = 'Searching for: ' + req.body.name + '<br>';
+    var found = mushroom.find(req.body.name);
+    if (found){
+        res.send(header + "Found:" + found.length);
+    } else {
+        res.send(header + "Not found");
+    }
+    res.render("details", {name: req.body.name, result: found});
+});
+
+
+// define 404 handler
+app.use( (req,res) => {
+ res.type('text/plain'); 
+ res.status(404);
+ res.send('404 - Not found');
+});
+
+app.listen(app.get('port'), () => {
+ console.log('Express started'); 
+});
 
