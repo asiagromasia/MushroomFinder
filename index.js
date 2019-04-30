ab9693ea3b557cc4b0a8f6a8026a9ed455f4453e
Asia <Joanna.gromadzka@seattlecentral.edu>
2019-04-29 23:53:17 -0700

    assigment3- extra credit

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 'use strict'
 let mushrooms = require("./lib/mushrooms.js");
+const query = require('querystring');
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 4000);
 app.use(express.static(__dirname + '/public')); // set location for static files
 app.use(bodyParser.urlencoded({extended: true})); // parse form submissions
 
@@ -14,15 +15,18 @@ app.set("view engine", ".html");
 
 // send static file as response
 app.get('/', (req, res) => {
+    const mush = mushrooms.getAll();
  res.type('text/html');
- res.sendFile(__dirname + '/public/home.html'); 
-  //  res.render('home');
+// res.sendFile(__dirname + '/public/home.html'); 
+    res.render('home', {mushrooms: mush});
 });
 
 // send plain text response
 app.get('/about', (req, res) => {
  res.type('text/plain');
- res.sendFile(__dirname + '/public/about.html');
+   const about2= mushrooms.getAll();
+//res.sendFile(__dirname + '/views/about.html');
+   res.render('about', {mushrooms: about2});
 });
 
 app.get('/getall', (req,res) => {
@@ -30,6 +34,12 @@ app.get('/getall', (req,res) => {
           res.writeHead(200, {'Content-Type': 'text/plain'});
           res.end('Here are all mushrooms: ' + "\n" + JSON.stringify(mushrCont));
 });      
+app.get('/get', (req,res) => {
+    let found = mushrooms.get(req.query.name) //get mushroom object
+    let results = (found) ? JSON.stringify(found) : " Item Not found";
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.render('details', {name: req.query.name, result: found})
+})
 
 // handle GET 
 app.get('/delete', (req,res) => {
@@ -41,7 +51,7 @@ app.get('/delete', (req,res) => {
 app.post('/search', (req,res) => {
     res.type('text/html');
     console.log(req.body) //display parsed form submission
-    var found = mushrooms.get(req.body.name);
+    let found = mushrooms.get(req.body.name);
     res.render("details", {name: req.body.name, result: found});
 });
 
--- test/test_main.js
+++ test/test_main.js
@@ -1,9 +1,9 @@
-var expect = require("chai").expect;
-var book = require("../lib/mushrooms");
+const expect = require("chai").expect;
+const mushrooms = require("../lib/mushrooms");
 const validatePassword = (password) => {
     return (password.toLowerCase() != password);
 }
-
+/*
 describe("Password validation", () => {
  it("passes if mixed case", () => {
    var result = validatePassword("agbBN");
@@ -14,16 +14,16 @@ describe("Password validation", () => {
    var result = validatePassword("fakkjhue");
    expect(result).to.be.false;
  });
-});
-/*
+}); */
+
 describe("Mushrooms module", () => {
  it("returns requested mushroom", () => {
-   var result = book.get("dune");
-   expect(result).to.deep.equal({title: "dune", author:"frank herbert", pubdate:1969});
+   const result = mushrooms.get("morel");
+   expect(result).to.deep.equal({name: "Morel", size: 3, location: "Mason Lake"});
  });
  
- it("fails w/ invalid book", () => {
-   var result = book.get("fake");
+ it("fails w/ invalid mushroom", () => {
+   const result = mushrooms.get("fake");
    expect(result).to.be.undefined;
  });
-}); */
+}); 
