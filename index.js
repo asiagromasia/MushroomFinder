03d614f5a94af486ae6098e34d9ae9001934ea4d
Asia <Joanna.gromadzka@seattlecentral.edu>
2019-05-15 13:39:51 -0700

    Assigment 5 final version

--- index.js
+++ index.js
@@ -21,74 +21,67 @@ app.get('/', (req, res, next) => {
         mushroom.find({}, (err, mush) => {
           if (err) return next(err);
             //console.log(mushroom.length);
-           // console.log(mush);
+            //åconsole.log(mush);
             res.type('text/html');
             res.render('home', {mushrooms: mush, wantDisplay: false});
             
          });     
 });
 
+//+5 send plain text response
+app.get('/about', (req, res) => {
+        //res.send('a get request with /about route on port 4000 ');
+        res.type('text/html');
+        res.render('about');
+        //res.redirect('http://www.expressjs.com/en/guide/using-middleware.html');
+});
+
 app.get('/getall', (req,res, next) => {
        mushroom.find({}, (err, mush) => {
          if (err) return next(err);
-           console.log(mush.length); 
-        res.type('text/html');
-        res.render('home', {mushrooms: mush, wantDisplay: true});
-     });
-     });
-    
-
-//+5 send plain text response
-app.get('/about', (req, res) => {
-    //res.send('a get request with /about route on port 4000 ');
-    res.type('text/html');
-    res.render('about');
-    //res.redirect('http://www.expressjs.com/en/guide/using-middleware.html');
+           console.log(mush.length);
+           res.type('text/html');
+           res.render('home', {mushrooms: mush, wantDisplay: true});
+        });
 });
 
-
-//+5 return a single record
+//+5 return a single record post(body)
 app.post('/search', (req,res, next) => {
-    console.log(req.body);
+        console.log(req.body);
         mushroom.findOne({'name':req.body.name}, (err, name) => {
           if (err) return next(err);
-          console.log(mushroom);
             res.type('text/html');
-            res.render("details", {result:name});
+            res.render("details", {result:name, justAdded: false});
         }); 
      }); 
 
-//5 handle GET 
+//5 handle GET (query)
 app.get('/delete', (req,res, next) => {
-    mushroom.deleteOne({'name':req.query.name}, (err,name) => {
-        if(err) return next(err);
-      
-        console.log(name);
-        console.log(req.query.name);
-        mushroom.countDocuments((err, total) => {   
-   // mushroom.countDocuments((err, total) => {   
-        res.type('text/html');
-        res.render('delete',  {name: req.query.name, result: name, total: total }); 
-    });
-    
-  });
-});
-// insert or update a single record
-//it is adding but not showing it is added
-app.post('/add', (req, res, next) => {
-    let newMushroom = {"name":req.body.name, "size":req.body.size, "location":req.body.location};
-    mushroom.update({'name': req.body.name}, newMushroom, {upsert:true}, (err, result) => {
-      if (err) return next(err);
-      console.log(result); 
+        mushroom.deleteOne({'name':req.query.name}, (err,name) => {
+            if(err) return next(err);
+            console.log(name);
+            console.log(req.query.name);
+        mushroom.countDocuments((err, total) => {
+               let toDel = {"name": req.query.name};
+                res.type('text/html');
+                res.render('delete', {result: toDel, total: total});  
+        });
 
-    mushroom.find({}, (err, items) =>{
-        if (err) return next (err);
-        console.log(items.length);
-        res.type('text/html');
-        res.render('home');
-    });
-        
-    });
+        });
+});
+
+// insert or update a single record
+app.post('/add', (req, res, next) => {
+        let newMushroom = {"name":req.body.name, "size":req.body.size, "location":req.body.location};
+        mushroom.update({'name': req.body.name}, newMushroom, {upsert: true}, (err, result) => {    if (err) return next(err);
+            console.log(result); 
+        mushroom.find({}, (err, items) =>{
+            if (err) return next (err);
+                console.log(items.length);
+            res.type('text/html');
+            res.render("details", {result: newMushroom, justAdded: true });
+        });
+        });
 });
  
 
